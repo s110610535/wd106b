1eab0b1df31a53c333f54232deaea30b9face87d
s110610535 <36975430+s110610535@users.noreply.github.com>
2018-03-30 21:47:25 +0800

    Add files via upload

---
+++ exercise/0330-1.js
@@ -0,0 +1,9 @@
+var array;
+array = 1;
+
+for(i = 1;i < 11;i += 1)
+{
+    array *= i;
+    console.log(array);
+}
+
---
+++ exercise/0330-2.js
@@ -0,0 +1,9 @@
+var i = 0, j = 0, x = 0;
+for(i = 1;i < 11;i += 1)
+{
+    for(j = 1;j < 11;j += 1)
+    {
+        x = i*j;
+        console.log(i +'*'+ j + '=' + x);
+    }
+}
